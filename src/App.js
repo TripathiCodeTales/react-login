5866918f321ea66f5e0864e8ffbdef26f746d121
TripathiCodeTales <133916225+TripathiCodeTales@users.noreply.github.com>
2023-06-15 19:04:21 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import React from "react";
-import "./style.css";
-import Form from "./Form";
-import Message from "./Message";
-import Submit from "./Submit";
+import React from 'react';
+import './style.css';
+import Form from './Form';
+import Message from './Message';
+import Submit from './Submit';
 export default function App() {
-
-
   return (
     <>
-      <h1>learn how hands-on react.js projects can get you a job with ease!</h1>
-      <p>Avail your free 1:1 mentoring sessions</p>
-    <Form />
-    <Message />
-    <Submit />
-     
-    
-    
+      <Form />
+      <Message />
+      <Submit />
     </>
   );
-
 }
-
